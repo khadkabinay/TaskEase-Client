c872e6f8f099615466ceac72d75e5f717d1a0cd7
Binay bk <khadkabinay@yahoo.com>
2020-10-27 14:57:29 -0500

    Adds home page

--- my-app/src/config/Routes.js
+++ my-app/src/config/Routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
+import AdminHome from '../pages/AdminHome'
 
 
 const Routes = (props) => {
     return (
       <Switch>
         <Route exact path='/' component={ Home } />
+        <Route  path='/users' component={ AdminHome } />
       </Switch>
     )
   }
