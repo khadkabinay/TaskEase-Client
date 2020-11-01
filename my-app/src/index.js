e227c0f0bed7986d79dd3db9e301504f4b79e81e
Binay bk <khadkabinay@yahoo.com>
2020-10-31 20:09:19 -0500

    Adds atoms.js and seletors.js on recoil folder

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {RecoilRoot} from "recoil"
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
 
-<Router>  
-    <App />
-</Router>
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
 
 ,document.getElementById('root'));
 
