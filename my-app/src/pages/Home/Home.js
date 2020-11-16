2660a0e5b612308735a3613f4c87457dad192afe
Binay bk <khadkabinay@yahoo.com>
2020-11-16 00:24:52 -0600

    Changes background of home page

--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css'
+import Footer from '../../components/Footer/Footer'
 const Home = () => {
   return (
     <div>
-       
-      <h1 className='home-title-text fas fa-tasks'>&nbsp;Welcome To TaskEase</h1>
+    <h1 className='home-title-text fas fa-tasks'>&nbsp;Welcome To TaskEase</h1>
+    <div className='task-pic'></div>
+    <Footer/>
     </div>
   );
 }
